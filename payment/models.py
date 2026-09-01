a818c672984ffbf2231a7d07304d2f059834483d
Ebrima123 <esawaneh23@gmail.com>
2026-09-01 06:40:52 +0000

    Make payment field optional in Receipt model

--- payment/models.py
+++ payment/models.py
@@ -90,7 +90,7 @@ class Transaction(models.Model):
 
 
 class Receipt(models.Model):
-    payment = models.OneToOneField(Payment, on_delete=models.CASCADE, related_name='receipt')
+    payment = models.OneToOneField(Payment, on_delete=models.CASCADE, related_name='receipt', null=True, blank=True)
     signatory = models.ForeignKey('settings_app.Signatory', on_delete=models.SET_NULL, null=True, blank=True)
     receipt_number = models.CharField(max_length=50, unique=True, db_index=True)
 
